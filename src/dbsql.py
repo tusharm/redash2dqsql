1921024474d8be3b11f75115ec39d030f40b27d5
Tushar Madhukar <tushar.madhukar@gmail.com>
2023-12-05 17:14:59 +1100

    Updating table names in the source query

--- src/dbsql.py
+++ src/dbsql.py
@@ -1,4 +1,4 @@
-from sqlglot import transpile
+from sqlglot import transpile, parse_one, exp
 
 from redash import Query
 
@@ -10,7 +10,27 @@ def convert_from(query: Query, dialect='presto'):
     result = transpiled[0]
 
     # TODO: this is a hack. need to check if this is true only for PRESTO dialect
-    for param in query.params:
-        result = result.replace(f"STRUCT(STRUCT({param}))", f"{{{{{param}}}}}")
+    result = fix_schema_names(result)
+    result = fix_query_params(result, query.params)
 
     return result
+
+
+def fix_query_params(query: str, params):
+    result = query
+    for param in params:
+        result = result.replace(f"STRUCT(STRUCT({param}))", f"{{{{{param}}}}}")
+    return result
+
+
+def fix_schema_names(query: str, dialect='databricks'):
+    # Parse the SQL statement
+    expression_tree = parse_one(query, read=dialect)
+
+    def transformer(node):
+        if isinstance(node, exp.Table) and len(node.parts) > 1:
+            return exp.Table(this=node.name, db=node.db, catalog="hive_metastore", alias=node.alias)
+        return node
+
+    transformed_tree = expression_tree.transform(transformer)
+    return transformed_tree.sql(pretty=True, dialect=dialect)
